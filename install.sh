f8f9a780778343507d65896ccc6500f3a9bad201
NACSAdmin <admin@nacsglobal.com>
2021-11-01 13:51:06 -0400

    first commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update && sudo apt full-upgrade -y
+sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list"
+sudo apt update
+wget 'https://archive.kali.org/archive-key.asc'
+sudo apt-key add archive-key.asc
+sudo apt update
+sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
+sudo apt update
+
+echo "Kali repository installed. To install kali package use \"sudo aptitude install -t kali-rolling <PACKAGE>\""
